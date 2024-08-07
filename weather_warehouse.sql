5b7e3ccce17a675b405031d9e89949530a54af76
WCM-CS <walker.educs@gmail.com>
2024-08-07 02:45:14 -0400

    Change order of table creation to make dim tables before the fact table

--- weather_warehouse.sql
+++ weather_warehouse.sql
@@ -1,18 +1,5 @@
 USE WeatherData;
 
-CREATE TABLE Station (
-    Station_ID CHAR(4) PRIMARY KEY,
-    Latitude FLOAT(10),
-    Longitude FLOAT(10),
-    City VARCHAR(15),
-    State CHAR(2),
-    Country CHAR(2),
-    Weather_ID INT,
-    Pollutant_ID INT,
-    FOREIGN KEY (Weather_ID) REFERENCES Weather(Weather_ID),
-    FOREIGN KEY (Pollutant_ID) REFERENCES Pollutants(Pollutant_ID)
-);
-
 CREATE TABLE Weather (
     Weather_ID INT AUTO_INCREMENT PRIMARY KEY,
     Temperature_F FLOAT(5, 2),
@@ -30,4 +17,17 @@ CREATE TABLE Pollutants (
     Sulfur_Dioxide FLOAT(5, 2),
     Particulate_Matter FLOAT(5, 2),
     Ammonia FLOAT(5, 2)
+);
+
+CREATE TABLE Station (
+    Station_ID CHAR(4) PRIMARY KEY,
+    Latitude FLOAT(10),
+    Longitude FLOAT(10),
+    City VARCHAR(15),
+    State CHAR(2),
+    Country CHAR(2),
+    Weather_ID INT,
+    Pollutant_ID INT,
+    FOREIGN KEY (Weather_ID) REFERENCES Weather(Weather_ID),
+    FOREIGN KEY (Pollutant_ID) REFERENCES Pollutants(Pollutant_ID)
 );
